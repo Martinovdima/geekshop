15a53b1ffb03ac4b6b24507bfdd4021859988588
Martinovdima <martinovdima@rambler.ru>
2021-06-04 13:08:50 +0600

    added __str__ in models.py register "ProductCategory" and "Product" in admin.py

--- products/models.py
+++ products/models.py
@@ -7,6 +7,10 @@ class ProductCategory(models.Model):
     description = models.TextField(blank=True, null=True)
 
 
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=256)
     image = models.ImageField(upload_to='product_image', blank=True)
@@ -14,3 +18,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8,decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
