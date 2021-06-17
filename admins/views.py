7ee92925c35276de438598c0ed2240e1cc9c0bb2
Martinovdima <martinovdima@rambler.ru>
2021-06-17 13:10:18 +0600

    added create for admins

--- admins/views.py
+++ admins/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 from users.models import User
+from admins.forms import UserAdminRegisterForm
 
 
 def index(request):
     return render(request, 'admins/admin.html')
 
 def admin_users(request):
-    content = {'users': User.objects.all()}
+    content = {'title': 'Geekshop - Админ | Пользователи','users': User.objects.all()}
     return render(request, 'admins/admin-users-read.html', content)
 
 def admin_users_create(request):
-    return render(request, 'admins/admin-users-create.html')
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegisterForm()
+    content = {'title': 'Geekshop - Админ | Регистрация', 'form': form}
+    return render(request, 'admins/admin-users-create.html', content)
 
 def admin_users_update(request, id):
     return render(request, 'admins/admin-users-update-delete.html')
