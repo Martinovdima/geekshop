2e9245bbb9997de9df36f4557ee48e4689f80cf0
Martinovdima <martinovdima@rambler.ru>
2021-06-21 16:05:07 +0600

    added pagination for Products

--- products/views.py
+++ products/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 import os
-import json
 from products.models import ProductCategory, Product
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 MODULE_DIR = os.path.dirname(__file__)
@@ -13,12 +12,16 @@ def index(request):
     return render(request, 'products/index.html', content)
 
 
-def products(request, category_id=None):
-    content = {
-        'categories': ProductCategory.objects.all(),
-        'title': 'GeekShop - каталог',
-    }
-    content.update({
-        'products': Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    })
+def products(request, category_id=None, page=1):
+    content = {'categories': ProductCategory.objects.all(),'title': 'GeekShop - каталог',}
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    content['products'] = products_paginator
     return render(request, 'products/products.html', content)
