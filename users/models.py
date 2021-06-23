34ff86747e0827dcc1be3bce4bf9c01398dc3474
Martinovdima <martinovdima@rambler.ru>
2021-06-23 16:48:12 +0600

    added UserLogoutView, UserRegisterView, UserProfileView for Users

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True)
+
+    def get_absolute_url(self):
+        return reverse('users:profile', kwargs={'pk': self.pk})
